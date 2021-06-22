f59d81183e9dd5a722c1c9214b94ffa70f4fc3e8
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2021-06-21 23:26:44 -0300

    Modified scripts origin

--- src/mfe-archetype/files/react/no-routing/fe-obe-__name@dasherize__/scripts/container-dev.sh
+++ src/mfe-archetype/files/react/no-routing/fe-obe-__name@dasherize__/scripts/container-dev.sh
@@ -1,2 +1,3 @@
-docker build -t fe-obe-container-dev -f ../container/Dockerfile.dev ../container
+#container build & run
+docker build -t fe-obe-container-dev ../container/Dockerfile.dev ../container
 docker run --rm -p 3008:3008 fe-obe-container-dev
