7b6af9c86f83deb935eceeab5d7dc125bd1b4518
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2021-06-21 23:52:54 -0300

    Fixed Dockerfile.dev typo

--- src/mfe-archetype/files/angular/routing/fe-obe-__name@dasherize__/scripts/container-dev.sh
+++ src/mfe-archetype/files/angular/routing/fe-obe-__name@dasherize__/scripts/container-dev.sh
@@ -1,3 +1,3 @@
 #container build & run
-docker build -t fe-obe-container-dev ../container/Dockerfile.dev ../container
+docker build -t fe-obe-container-dev -f ../container/Dockerfile.dev ../container
 docker run --rm -p 3008:3008 fe-obe-container-dev
