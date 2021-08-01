8834280755db2daa7290d89c0857c8f8e4e1cbda
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2021-08-01 19:19:06 -0300

    Fixed delete-mfe bug pathname

--- src/mfe-archetype/delete-mfe.js
+++ src/mfe-archetype/delete-mfe.js
@@ -7,12 +7,12 @@ const schematics_1 = require("@angular-devkit/schematics");
 const helper_1 = require("./helper");
 function deleteMFE(_options) {
     return (tree, _context) => {
-        const { name, port } = _options;
+        const { name, port, route } = _options;
         helper_1.validatePort(port);
-        helper_1.deleteFromFile("./src/App.js", `<Route path="/${name}" component={${strings_1.capitalize(name)}Lazy} />`, tree);
-        helper_1.deleteFromFile("./src/App.js", `const ${strings_1.capitalize(name)}Lazy = lazy(() => import("./components/${strings_1.capitalize(name)}App"));`, tree);
-        helper_1.deleteFromFile("./config/webpack.dev.js", `${strings_1.camelize(name)}: "${strings_1.camelize(name)}@http://localhost:${port}/remoteEntry.js",`, tree);
-        helper_1.deleteFromFile("./config/webpack.prod.js", `${strings_1.camelize(name)}: \`${strings_1.camelize(name)}@\${${name}_domain}/remoteEntry.js\`,`, tree);
+        helper_1.deleteFromFile("./src/App.js", `<Route path="/${route}" component={${strings_1.classify(name)}Lazy} /> \n`, tree);
+        helper_1.deleteFromFile("./src/App.js", `const ${strings_1.classify(name)}Lazy = lazy(() => import("./components/${strings_1.classify(name)}App")); \n`, tree);
+        helper_1.deleteFromFile("./config/webpack.dev.js", `${strings_1.camelize(name)}: "${strings_1.camelize(name)}@http://localhost:${port}/remoteEntry.js", \n`, tree);
+        helper_1.deleteFromFile("./config/webpack.prod.js", `${strings_1.camelize(name)}: \`${strings_1.camelize(name)}@/obe/modulos/${name}/remoteEntry.js\`, \n`, tree);
         const oldFilePath = core_1.normalize(`./src/components/${strings_1.classify(name)}App.js`);
         const oldFileBuffer = tree.read(oldFilePath);
         if (!oldFileBuffer) {
